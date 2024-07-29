fbd65b5aa1ef91ed75d57c75cb5f3eeaee1eaf08
leolee9086 <464803712@qq.com>
2024-07-30 03:34:11 +0800

    重构代码:重构本地文件夹数据获取流程

--- source/data/galleryDefine.js
+++ source/data/galleryDefine.js
@@ -77,3 +77,63 @@ export  async function applyStmt(stmt) {
         }
     )
 }
+export async function applyURIStreamJson(uri, target, callback, step, signal) {
+    let _step = 0;
+    fetch(uri, { signal })
+    .then(response => {
+        if (response.ok) {
+            // 获取响应的可读流
+            const reader = response.body.getReader();
+            return new ReadableStream({
+                start(controller) {
+                    function push() {
+                        reader.read().then(({ value, done }) => {
+                            if (done) {
+                                controller.close();
+                                return;
+                            }
+                            // 将每个文件信息推送到流中
+                            controller.enqueue(new TextDecoder('utf-8').decode(value));
+                            push(); // 继续读取下一部分
+                        });
+                    }
+                    push();
+                }
+            });
+        } else {
+            throw new Error('Network response was not ok.');
+        }
+    })
+    .then(stream => {
+        // 使用流式处理读取数据
+        const reader = stream.getReader();
+        read()
+        let splitedChunk
+        function read() {
+            reader.read().then(({ value, done }) => {
+                if (done) {
+                    callback && callback()
+                    console.log('Stream complete');
+                    return;
+                }
+                value.split('\n').forEach(chunk => {
+                    try {
+                        splitedChunk ? target.push(JSON.parse(splitedChunk + chunk)) : target.push(JSON.parse(chunk))
+                        _step += 1
+                        if (_step >= step) {
+                            callback && callback()
+                            _step = 0
+                        }
+                        splitedChunk = undefined
+                    } catch (e) {
+                        splitedChunk = chunk
+                    }
+                });
+                // 处理文件信息
+                // 继续读取
+                read();
+            })
+        }
+    }).catch(error => { console.error(error) })
+
+}
--- source/data/siyuanAssets.js
+++ source/data/siyuanAssets.js
@@ -1,6 +1,6 @@
 import { cleanAssetPath } from "./utils/assetsName.js"
 import { plugin } from '../asyncModules.js'
-import { applyStmt } from './galleryDefine.js'
+import { applyStmt,applyURIStreamJson } from './galleryDefine.js'
 window[Symbol.for('$pathCache')]=window[Symbol.for('$pathCache')]||{}
 export const pathCache =window[Symbol.for('$pathCache')]
 export async function 获取tab附件数据(tab, limit, offset) {
@@ -16,61 +16,6 @@ export async function 获取tab附件数据(tab, limit, offset) {
     return data
 }
 export async function 获取本地文件夹数据(globSetting, target, callback, step, signal) {
-    let _step = 0
-    fetch(`http://localhost:${plugin.http服务端口号}/glob-stream?setting=${encodeURIComponent(JSON.stringify(globSetting))}`, { signal })
-        .then(response => {
-            if (response.ok) {
-                // 获取响应的可读流
-                const reader = response.body.getReader();
-                return new ReadableStream({
-                    start(controller) {
-                        function push() {
-                            reader.read().then(({ value, done }) => {
-                                if (done) {
-                                    controller.close();
-                                    return;
-                                }
-                                // 将每个文件信息推送到流中
-                                controller.enqueue(new TextDecoder('utf-8').decode(value));
-                                push(); // 继续读取下一部分
-                            });
-                        }
-                        push();
-                    }
-                });
-            } else {
-                throw new Error('Network response was not ok.');
-            }
-        })
-        .then(stream => {
-            // 使用流式处理读取数据
-            const reader = stream.getReader();
-            read()
-            let splitedChunk
-            function read() {
-                reader.read().then(({ value, done }) => {
-                    if (done) {
-                        callback && callback()
-                        console.log('Stream complete');
-                        return;
-                    }
-                    value.split('\n').forEach(chunk => {
-                        try {
-                            splitedChunk ? target.push(JSON.parse(splitedChunk + chunk)) : target.push(JSON.parse(chunk))
-                            _step += 1
-                            if (_step >= step) {
-                                callback && callback()
-                                _step = 0
-                            }
-                            splitedChunk = undefined
-                        } catch (e) {
-                            splitedChunk = chunk
-                        }
-                    });
-                    // 处理文件信息
-                    // 继续读取
-                    read();
-                })
-            }
-        }).catch(error => { console.error(error) })
+    let uri = `http://localhost:${plugin.http服务端口号}/glob-stream?setting=${encodeURIComponent(JSON.stringify(globSetting))}`
+    applyURIStreamJson(uri, target, callback, step, signal)
 }
