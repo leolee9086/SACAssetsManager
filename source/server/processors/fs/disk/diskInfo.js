68665034dbd634316e3a0386185694dd25ee7140
leolee9086 <464803712@qq.com>
2024-08-16 18:51:50 +0800

    清理多余代码

--- source/server/processors/fs/disk/diskInfo.js
+++ source/server/processors/fs/disk/diskInfo.js
@@ -13,6 +13,7 @@ export function listLocalDisks() {
         let diskInfos = []
         const platform = process.platform;
         if (platform === 'win32') {
+            // Windows平台
             try{
                 diskInfos = listLocalDisksWin32(outputFilePath)
                 resolve(diskInfos)
@@ -21,40 +22,6 @@ export function listLocalDisks() {
                 reject(e)
             }
             return
-            // Windows平台
-            const command = `chcp 65001 && wmic logicaldisk get DeviceID,VolumeName,FileSystem,Size,FreeSpace >${outputFilePath} `;
-            exec(command, { encoding: 'utf8' }, (error, stdout) => {
-                if (error) {
-                    console.error(`exec error: ${error}`);
-                    reject(error)
-                }
-                // 获取输出中每个磁盘名称，通常是C:, D: 等
-                const output = fs.readFileSync(outputFilePath,'utf16le')
-                const lines = output.trim().split('\n').slice(1);
-
-                lines.forEach(line => {
-                    const [deviceId,  fileSystem, freeSpace,size, volumeName] = line.trim().split(/\s+/);
-                    console.log(deviceId,  fileSystem, size, freeSpace,volumeName)
-                    if (existsSync(deviceId + "/")) {
-                        try {
-                            const totalMB = parseInt(size) / (1024 * 1024);
-                            const freeMB = parseInt(freeSpace) / (1024 * 1024);
-                            diskInfos.push({
-                                name: deviceId.trim(),
-                                volumeName: volumeName&&volumeName.trim() || '本地磁盘', // 如果没有卷标，使用默认名称
-                                Filesystem: fileSystem,
-                                total: totalMB,
-                                free: freeMB,
-                                usedPercentage: ((totalMB - freeMB) / totalMB) * 100
-                            });
-                            console.log(diskInfos)
-                        } catch (e) {
-                            console.error(e);
-                        }
-                    }
-                });
-                resolve(diskInfos);
-            });
         } else {
             // Unix-like平台
             const command = 'df -P | tail -n +2';
