d8397c064cf7122be1b0b32ba391f21707a72701
leolee9086 <464803712@qq.com>
2024-08-16 18:49:09 +0800

    重构代码

--- source/server/processors/fs/disk/diskInfo.js
+++ source/server/processors/fs/disk/diskInfo.js
@@ -1,12 +1,65 @@
 const { exec } = window.require('child_process');
 const { statfsSync, existsSync } = window.require('fs');
 const outputFilePath = require('path').join(siyuan.config.system.workspaceDir, 'temp','sac','wmic_output.txt');
+
+/**
+ * windows平台下调用这个函数
+ * 调用wmic命令获取磁盘信息
+ * @param {string} outputFilePath 输出文件路径
+ * @returns {Array} 磁盘信息
+ */
+function listLocalDisksWin32(outputFilePath){
+    let diskInfos = []
+    const command = `chcp 65001 && wmic logicaldisk get DeviceID,VolumeName,FileSystem,Size,FreeSpace >${outputFilePath} `;
+    exec(command, { encoding: 'utf8' }, (error, stdout) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            throw error
+        }
+    });
+    const output = fs.readFileSync(outputFilePath,'utf16le')
+    const lines = output.trim().split('\n').slice(1);
+    lines.forEach(line => {
+        const [deviceId,  fileSystem, freeSpace,size, volumeName] = line.trim().split(/\s+/);
+        console.log(deviceId,  fileSystem, size, freeSpace,volumeName)
+        if (existsSync(deviceId + "/")) {
+            try {
+                const totalMB = parseInt(size) / (1024 * 1024);
+                const freeMB = parseInt(freeSpace) / (1024 * 1024);
+                diskInfos.push({
+                    name: deviceId.trim(),
+                    volumeName: volumeName&&volumeName.trim() || '本地磁盘', // 如果没有卷标，使用默认名称
+                    Filesystem: fileSystem,
+                    total: totalMB,
+                    free: freeMB,
+                    usedPercentage: ((totalMB - freeMB) / totalMB) * 100
+                });
+                console.log(diskInfos)
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    });
+    return diskInfos
+}
+
+
+
+
 export function listLocalDisks() {
     return new Promise((resolve, reject) => {
         let disks = [];
         let diskInfos = []
         const platform = process.platform;
         if (platform === 'win32') {
+            try{
+                diskInfos = listLocalDisksWin32(outputFilePath)
+                resolve(diskInfos)
+            }catch(e){
+                console.error(e)
+                reject(e)
+            }
+            return
             // Windows平台
             const command = `chcp 65001 && wmic logicaldisk get DeviceID,VolumeName,FileSystem,Size,FreeSpace >${outputFilePath} `;
             exec(command, { encoding: 'utf8' }, (error, stdout) => {
