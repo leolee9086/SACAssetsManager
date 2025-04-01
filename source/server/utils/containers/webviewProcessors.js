f345bba74c258bff78ae4c8649227f66f4b342ca
leolee9086 <464803712@qq.com>
2025-04-01 14:01:59 +0800

    改进服务端项目组织

--- source/server/utils/containers/webviewProcessors.js
+++ source/server/utils/containers/webviewProcessors.js
@@ -1,38 +1,9 @@
-import { createWebviewByJsURL } from "./webview.js"
-export async function exposeFunctionToWebview( functionImpl) {
-    const jsString = functionImpl.toString()
-    const wrapedjsString=`
-    const electron=require('@electron/remote')
-    const {ipcRenderer}=electron
-    ipcRenderer.on('invokeFunction', ($event,data) => {
-        const sender = $event.sender
-        const {timeStamp,args}=data
-        const  result = ${jsString}.apply(null,args)
-        require('@electron/remote').ipcRenderer.send('invokeFunctionResult', {timeStamp,result})
-    });
-    `
-    const blob = new Blob([wrapedjsString], { type: 'application/javascript' });
-    const url = URL.createObjectURL(blob);
-    const webview = await createWebviewByJsURL(url);
-    console.log(webview)
-    webview.openDevTools()
-    return (...args)=>{
-        return new Promise((resolve,reject)=>{
-            const timeStamp = Date.now()
-            const webviewId = webview.getWebContentsId()
-            const webviewWebContents = webContents.fromId(webviewId)
-            webviewWebContents.send('invokeFunction', {timeStamp,args})
-            require('@electron/remote').ipcRenderer.on('invokeFunctionResult', (event, result) => {
-                if(result.timeStamp===timeStamp){
-                    resolve(result.result)
-                }
-                setTimeout(()=>{
-                    reject(new Error('invokeFunction timeout'))
-                },1000)
-            })
-        })
-    }
+import { 通过JS地址创建Webview, 向Webview暴露函数 } from "../../../../src/toolBox/base/useElectron/forWindow/useWebview.js"
+
+export async function exposeFunctionToWebview(functionImpl) {
+    return await 向Webview暴露函数(functionImpl);
 }
+
 //测试
 function test(){
     return new Promise((resolve,reject)=>{
@@ -41,7 +12,7 @@ function test(){
         },1000)
     })
 }
-let testFunction =await exposeFunctionToWebview(test)
+let testFunction = await 向Webview暴露函数(test)
 console.log(testFunction)
 setInterval(()=>{
     testFunction().then((result)=>{
--- source/utilWebviews/drag.js
+++ source/utilWebviews/drag.js
@@ -2,5 +2,5 @@
  * 单独创建一个webview用于触发原生拖拽
  */
 import { plugin } from '../pluginSymbolRegistry.js';
-import { createInvisibleWebview } from '../server/utils/containers/webview.js';
-plugin.dragListenerWebview =await createInvisibleWebview(import.meta.resolve('./drag.html'))
+import { 创建不可见Webview } from '../../src/toolBox/base/useElectron/forWindow/useWebview.js';
+plugin.dragListenerWebview = await 创建不可见Webview(import.meta.resolve('./drag.html'))
